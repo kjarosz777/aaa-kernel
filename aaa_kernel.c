584892a7bd1ee944384c41b62bebed658751f8e4
Kamil.Jarosz <kamil.rafal.jarosz@gmail.com>
2022-08-11 13:24:25 +0200

    VMA impl

--- aaa_kernel.c
+++ aaa_kernel.c
@@ -4,6 +4,7 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/rtc.h>
+#include <linux/mm.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
 
@@ -49,7 +50,7 @@ static ssize_t my_read(struct file* filp, char* buffer, size_t count, loff_t* po
 
   currentReadIndex %= bufferSize;
 
-  if( copy_to_user(buffer, pipeBuffer, sizeof(int)) !=0 ){
+  if( copy_to_user(buffer, pipeBuffer + currentReadIndex, sizeof(int)) !=0 ){
 		printk(KERN_WARNING "ERROR: copy_to_user did not write all bytes to user buffer\n");
 		return ENOMEM;
 	}else{
@@ -71,7 +72,7 @@ static ssize_t my_write(struct file* file, const char __user* buffer, size_t cou
 
   currentWriteIndex %= bufferSize;
 	/*Write to the currentWriteIndex - th row, one char in a column at a time */
-	if( copy_from_user(pipeBuffer, buffer, sizeof(int)) != 0){
+	if( copy_from_user(pipeBuffer + currentWriteIndex, buffer, sizeof(int)) != 0){
 			/* Error did not write all bytes*/
 		printk(KERN_WARNING "ERROR: copy_from_user did NOT read all bytes from user buffer\n");
 		return ENOMEM; 
@@ -84,12 +85,82 @@ static ssize_t my_write(struct file* file, const char __user* buffer, size_t cou
   return count;
 }
 
+void my_vma_open(struct vm_area_struct *vma)
+{
+  printk(KERN_NOTICE "My VMA open, virt %lx, phys %lx\n",
+    vma->vm_start, vma->vm_pgoff << PAGE_SHIFT);
+}
+void my_vma_close(struct vm_area_struct *vma)
+{
+  printk(KERN_NOTICE "My VMA close.\n");
+}
+
+static struct vm_operations_struct my_vm_ops =
+{
+    .open = my_vma_open,
+    .close = my_vma_close,
+};
+
 int my_mmap (struct file* file, struct vm_area_struct * vma)
 {
   // vma->vm_start = (unsigned long)(pipeBuffer);
   // vma->vm_end = (unsigned long)(pipeBuffer + bufferSize);
 
-  return 0;
+  // printk(KERN_NOTICE "BEFORE VMA open, virt %lx, phys %lx\n",
+  //   vma->vm_start, vma->vm_pgoff << PAGE_SHIFT);
+
+  // int ret = remap_pfn_range(vma, vma->vm_start, vma->vm_pgoff,
+  //   vma->vm_end - vma->vm_start,
+  //   vma->vm_page_prot);
+
+  // if (ret < 0)
+  // {
+  //   printk(KERN_ERR "remap_pfn_range failed, ret: %d\n", ret);
+  // }
+  // else
+  // {
+  //   printk(KERN_NOTICE "remap_pfn_range success, ret: %d\n", ret);
+  // }
+
+  // vma->vm_ops = &my_vm_ops;
+  // my_vma_open(vma);
+
+    int ret = 0;
+    struct page *page = NULL;
+    unsigned long size = (vma->vm_end - vma->vm_start);
+
+    if (size <= 0) {
+      return -EINVAL; 
+    } 
+
+    printk(KERN_NOTICE "size: %lu\n", size);
+
+    uintptr_t pipeBuffer2 = (uintptr_t)pipeBuffer;
+
+    printk(KERN_NOTICE "pipeBuffer: %p, pipeBuffer2: %lx, (*int)pipeBuffer2: %p, *int)pipeBuffer3: %lx, \n",
+      pipeBuffer, pipeBuffer2, (int*)(pipeBuffer2), (int*)(pipeBuffer2));
+
+    printk(KERN_NOTICE "pipeBuffer2 << PAGE_SHIFT: %lx\n", pipeBuffer2 << PAGE_SHIFT);
+
+    // page = virt_to_page((unsigned long)pipeBuffer + (vma->vm_pgoff << PAGE_SHIFT));
+    page = virt_to_page((pipeBuffer2 << PAGE_SHIFT));
+    
+    printk(KERN_NOTICE "vm_start %lx, vm_end %lx, vm_pgoff: %lx, phys %lx\n",
+      vma->vm_start, vma->vm_end, vma->vm_pgoff, vma->vm_pgoff << PAGE_SHIFT);
+    printk(KERN_NOTICE "pipeBuffer: %p, ULpipeBuffer: %lx, uintptr_t_pipeBuffer: %lx, page: %p\n",
+      pipeBuffer, (unsigned long)pipeBuffer, (uintptr_t)pipeBuffer, page);
+
+    ret = remap_pfn_range(vma, vma->vm_start, page_to_pfn(page), size, vma->vm_page_prot);
+    if (ret < 0)
+    {
+      printk(KERN_ERR "remap_pfn_range failed, ret: %d\n", ret);
+    }
+    else
+    {
+      printk(KERN_NOTICE "remap_pfn_range success, ret: %d\n", ret);
+    }
+
+  return ret;
 }
 
 static const struct file_operations my_fops = {
