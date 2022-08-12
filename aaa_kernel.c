d5885fcc9af577de73a7b567526abcea0e7d4fc6
Kamil.Jarosz <kamil.rafal.jarosz@gmail.com>
2022-08-12 15:21:55 +0200

    New approach part 2

--- aaa_kernel.c
+++ aaa_kernel.c
@@ -13,34 +13,16 @@ MODULE_AUTHOR("Gal Anonim");
 MODULE_DESCRIPTION("A simple Hello world LKM!");
 MODULE_VERSION("0.1");
 
-int * pipeBuffer;
 static int bufferSize;
 
 static int currentReadIndex = 0;
 static int currentWriteIndex = 0;
 
-enum { BUFFER_SIZE = 4 };
-
 struct mmap_info {
-	char *data;
+  int *data;
 };
 
-
-/* First page access. */
-static vm_fault_t vm_fault(struct vm_fault *vmf)
-{
-	struct page *page;
-	struct mmap_info *info;
-
-	pr_info("vm_fault\n");
-	info = (struct mmap_info *)vmf->vma->vm_private_data;
-	if (info->data) {
-		page = virt_to_page(info->data);
-		get_page(page);
-		vmf->page = page;
-	}
-	return 0;
-}
+struct mmap_info *info;
 
 /* Declarations for command line arguements to be passed to module
 	The module_param() macro takes 3 arguments: the name of the variable,
@@ -51,54 +33,41 @@ module_param(bufferSize, int, 0000);
 
 static int my_open(struct inode* inode, struct file* filp)
 {
-  // printk(KERN_INFO "Opened aaa_kernel misc char device\n");
-  // return 0;
-  struct mmap_info *info;
-
-	pr_info("open\n");
-	info = kmalloc(sizeof(struct mmap_info), GFP_KERNEL);
-	// pr_info("virt_to_phys = 0x%llx\n", (unsigned long long)virt_to_phys((void *)info));
-	info->data = (char *)get_zeroed_page(GFP_KERNEL);
-	memcpy(info->data, "asdf", BUFFER_SIZE);
-	filp->private_data = info;
-	return 0;
-}
-
-static int release(struct inode *inode, struct file *filp)
-{
-	struct mmap_info *info;
-
-	pr_info("release\n");
-	info = filp->private_data;
-	free_page((unsigned long)info->data);
-	kfree(info);
-	filp->private_data = NULL;
+	pr_info("my_open\n");
+	
+  filp->private_data = info;
 	return 0;
 }
 
 static int my_close(struct inode* inodep, struct file* filp)
 {
-  printk(KERN_INFO "Closed aaa_kernel misc char device\n");
+  pr_info("my_close\n");
+  
   return 0;
 }
 
 static ssize_t my_read(struct file* filp, char* buffer, size_t count, loff_t* position)
 {
-  printk(KERN_INFO "Using read() function of aaa_kernel in kernel space\n");
+  pr_info("my_read in kernel space\n");
 	
-  if(currentReadIndex > bufferSize){
-		printk(KERN_WARNING "Current index to read exceeds the size/bounds of the buffer EXITING\n");
+  if(currentReadIndex > bufferSize)
+  {
+		pr_warn("Current index to read exceeds the size/bounds of the buffer EXITING\n");
 		return ENOMEM; /*Not enough memory in buffer */
 	}
 
   currentReadIndex %= bufferSize;
 
-  if( copy_to_user(buffer, pipeBuffer + currentReadIndex, sizeof(int)) !=0 ){
-		printk(KERN_WARNING "ERROR: copy_to_user did not write all bytes to user buffer\n");
+  if(copy_to_user(buffer, info->data + currentReadIndex, sizeof(int)) != 0)
+  {
+		pr_warn("ERROR: copy_to_user did not write all bytes to user buffer\n");
 		return ENOMEM;
-	}else{
-		printk(KERN_WARNING "SUCCESS: copy_to_user wrote all bytes to user buffer\n");
 	}
+  else
+  {
+		pr_info("SUCCESS: copy_to_user wrote all bytes to user buffer\n");
+	}
+
 	++currentReadIndex;
 
   return count;
@@ -106,49 +75,68 @@ static ssize_t my_read(struct file* filp, char* buffer, size_t count, loff_t* po
 
 static ssize_t my_write(struct file* file, const char __user* buffer, size_t count, loff_t* ppos)
 {
-  printk(KERN_INFO "Using write() function of aaa_kernel module\n");
+  pr_info("my_write in kernel space\n");
 
-	if(currentWriteIndex > bufferSize){
-		printk(KERN_WARNING "Current index to write to exceeds bounds/size of buffer.\n");
+	if(currentWriteIndex > bufferSize)
+  {
+		pr_warn("Current index to write to exceeds bounds/size of buffer.\n");
 		return ENOMEM;
 	}
 
   currentWriteIndex %= bufferSize;
 	/*Write to the currentWriteIndex - th row, one char in a column at a time */
-	if( copy_from_user(pipeBuffer + currentWriteIndex, buffer, sizeof(int)) != 0){
+	if( copy_from_user(info->data + currentWriteIndex, buffer, sizeof(int)) != 0)
+  {
 			/* Error did not write all bytes*/
-		printk(KERN_WARNING "ERROR: copy_from_user did NOT read all bytes from user buffer\n");
+		pr_warn("ERROR: copy_from_user did NOT read all bytes from user buffer\n");
 		return ENOMEM; 
-	}else{
-		printk(KERN_NOTICE "SUCCESS: copy_from_user read all bytes from user buffer \n");
+	}
+  else
+  {
+		pr_info("SUCCESS: copy_from_user read all bytes from user buffer \n");
 	}
 
-  	++currentWriteIndex;
+  ++currentWriteIndex;
 
   return count;
 }
 
 void my_vma_open(struct vm_area_struct *vma)
 {
-  printk(KERN_NOTICE "My VMA open, virt %lx, phys %lx\n",
+  pr_info("My VMA open, virt %lx, phys %lx\n",
     vma->vm_start, vma->vm_pgoff << PAGE_SHIFT);
 }
 
 void my_vma_close(struct vm_area_struct *vma)
 {
-  printk(KERN_NOTICE "My VMA close.\n");
+  pr_info("My VMA close.\n");
+}
+
+/* First page access. */
+static vm_fault_t my_vm_fault(struct vm_fault *vmf)
+{
+	pr_info("my_vm_fault\n");
+	
+  info = (struct mmap_info *)vmf->vma->vm_private_data;
+	if (info->data)
+  {
+		struct page *page = virt_to_page(info->data);
+		get_page(page);
+		vmf->page = page;
+	}
+	return 0;
 }
 
 static struct vm_operations_struct my_vm_ops =
 {
     .open = my_vma_open,
     .close = my_vma_close,
-    .fault = vm_fault,
+    .fault = my_vm_fault,
 };
 
 static int my_mmap(struct file *filp, struct vm_area_struct *vma)
 {
-	pr_info("mmap\n");
+	pr_info("my_mmap\n");
 	vma->vm_ops = &my_vm_ops;
 	vma->vm_flags |= VM_DONTEXPAND | VM_DONTDUMP;
 	vma->vm_private_data = filp->private_data;
@@ -156,51 +144,9 @@ static int my_mmap(struct file *filp, struct vm_area_struct *vma)
 	return 0;
 }
 
-// int my_mmap (struct file* file, struct vm_area_struct * vma)
-// {
-//   // vma->vm_ops = &my_vm_ops;
-//   // my_vma_open(vma);
-
-//     int ret = 0;
-//     struct page *page = NULL;
-//     unsigned long size = (vma->vm_end - vma->vm_start);
-
-//     if (size <= 0) {
-//       return -EINVAL; 
-//     } 
-
-//     printk(KERN_NOTICE "size: %lu\n", size);
-
-//     uintptr_t pipeBuffer2 = (uintptr_t)pipeBuffer;
-
-//     printk(KERN_NOTICE "pipeBuffer: %p, pipeBuffer2: %lx, (*int)pipeBuffer2: %p, *int)pipeBuffer3: %lx, \n",
-//       pipeBuffer, pipeBuffer2, (int*)(pipeBuffer2), (int*)(pipeBuffer2));
-
-//     page = virt_to_page((unsigned long)pipeBuffer + (vma->vm_pgoff << PAGE_SHIFT));
-//     // page = virt_to_page((pipeBuffer2 << PAGE_SHIFT));
-    
-//     printk(KERN_NOTICE "vm_start %lx, vm_end %lx, vm_pgoff: %lx, phys %lx\n",
-//       vma->vm_start, vma->vm_end, vma->vm_pgoff, vma->vm_pgoff << PAGE_SHIFT);
-//     printk(KERN_NOTICE "pipeBuffer: %p, ULpipeBuffer: %lx, uintptr_t_pipeBuffer: %lx, page: %p\n",
-//       pipeBuffer, (unsigned long)pipeBuffer, (uintptr_t)pipeBuffer, page);
-
-//     ret = remap_pfn_range(vma, vma->vm_start, page_to_pfn(page), size, vma->vm_page_prot);
-//     if (ret < 0)
-//     {
-//       printk(KERN_ERR "remap_pfn_range failed, ret: %d\n", ret);
-//     }
-//     else
-//     {
-//       printk(KERN_NOTICE "remap_pfn_range success, ret: %d\n", ret);
-//     }
-
-//   return ret;
-// }
-
 static const struct file_operations my_fops = {
     .owner   = THIS_MODULE,
     .open    = my_open,
-    .release = release,
     .read    = my_read,
     .write   = my_write,
     .release = my_close,
@@ -217,32 +163,36 @@ static int __init hello_start(void)
 {
   int error;
 
-  printk(KERN_INFO "Loading hello module...\n");
-  printk(KERN_INFO "Hello world\n");
+  pr_info("Loading hello module...\n");
+  pr_info("Hello world\n");
 
   error = misc_register(&pipe_device);
   if (error)
   {
-    printk(KERN_ERR "Failed to register pipe device\n");
+    pr_err("Failed to register pipe device\n");
     return error;
   }
 
-  printk(KERN_NOTICE "Allocating buffer with size %d\n",bufferSize);
+  pr_info("Allocating buffer with size %d\n",bufferSize);
 
-  pipeBuffer = kmalloc(bufferSize * sizeof(int),GFP_KERNEL);
-  printk(KERN_NOTICE "Successfully created buffer\n");
+  info = kmalloc(sizeof(struct mmap_info), GFP_KERNEL);
+	info->data = (int *)get_zeroed_page(GFP_KERNEL);
+
+  pr_info("Successfully created buffer\n");
 
   return 0;
 }
 
 static void __exit hello_end(void)
 {
-  printk(KERN_INFO "Goodbye Mr.\n");
+  pr_info("Goodbye Mr.\n");
 
-  kfree(pipeBuffer);
-	printk(KERN_NOTICE "SUCCESSFULLY FREED NUMPIPE buffer\n");
+  free_page((unsigned long)info->data);
+  kfree(info);
+	pr_info("SUCCESSFULLY FREED NUMPIPE buffer\n");
+  
   misc_deregister(&pipe_device);
-	printk(KERN_NOTICE "SUCCESSFULLY DEREGISTERED NUMPIPE  misc device\n");
+	pr_info("SUCCESSFULLY DEREGISTERED NUMPIPE  misc device\n");
 }
 
 module_init(hello_start);
